4e88b88777c5309584de279e7f1b9391706d0fa3
TY <77572572+Zhao-Tian-yi@users.noreply.github.com>
2024-03-19 21:03:16 +0800

    Add files via upload

--- mmdet/models/custom/common_unique/unique_mask_generator3.py
+++ mmdet/models/custom/common_unique/unique_mask_generator3.py
@@ -54,6 +54,31 @@ class GMaskBinaryList(nn.Module):
         mask_list = self.g_mask_binary_list(x)
         return mask_list
 
+import torch
+import torch.nn.functional as F
+
+import torch
+import torch.nn.functional as F
+
+class MaskFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, mask_list_):
+        # 计算 mask
+        mask_list_topk ,topk_index = mask_list_.topk(320)
+        mask_list_min = torch.min(mask_list_topk, dim=1).values
+        mask_list_min_ = mask_list_min.unsqueeze(-1)
+        ge = torch.ge(mask_list_, mask_list_min_)
+        zero = torch.zeros_like(mask_list_)
+        one = torch.ones_like(mask_list_)
+        mask_list = torch.where(ge, one, zero)
+        return mask_list
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # 由于 mask 是根据 input_tensor 计算得来的，因此将梯度传递给 input_tensor
+        return grad_output
+
 
 @MODELS.register_module()
 class UniqueMaskGenerator3(BaseModule):
@@ -73,7 +98,8 @@ class UniqueMaskGenerator3(BaseModule):
         self.imgshape=imgshape
         self.Gmaskbinarylist_vis = GMaskBinaryList(imgshape)
         self.Gmaskbinarylist_lwir = GMaskBinaryList(imgshape)
-
+        self.MaskFun_vis = MaskFunction()
+        self.MaskFun_lwir = MaskFunction()
     def forward(self, img_vis, img_lwir):
         """Forward function."""
 
@@ -86,45 +112,55 @@ class UniqueMaskGenerator3(BaseModule):
         fre_m_lwir = torch.abs(lwir_fre)  # 幅度谱，求模得到
         fre_m_lwir = torch.fft.fftshift(fre_m_lwir)
         # fre_p_lwir = torch.angle(lwir_fre)  # 相位谱，求相角得到
-        # import pdb
-        # pdb.set_trace()
-        mask_vis_list = self.Gmaskbinarylist_vis(fre_m_vis)
-        mask_lwir_list = self.Gmaskbinarylist_lwir(fre_m_lwir)
+        mask_vis_list_ = self.Gmaskbinarylist_vis(fre_m_vis)
+        mask_lwir_list_ = self.Gmaskbinarylist_lwir(fre_m_lwir)
+        mask_vis_list = self.MaskFun_vis.apply(mask_vis_list_).reshape((-1, 1,self.patch_num , self.patch_num))
+        mask_vis_list[:, :, int(self.patch_num / 2) - 1:int(self.patch_num / 2) + 1,int(self.patch_num / 2) - 1:int(self.patch_num / 2) + 1] = 1
+        mask_lwir_list = self.MaskFun_lwir.apply(mask_lwir_list_).reshape((-1, 1,self.patch_num , self.patch_num))
+        mask_lwir_list[:, :, int(self.patch_num / 2) - 1:int(self.patch_num / 2) + 1,int(self.patch_num / 2) - 1:int(self.patch_num / 2) + 1] = 1
 
-        mask_vis_list_topk ,topk_index_vis = mask_vis_list.topk(320)
-        mask_lwir_list_topk,topk_index_lwir = mask_lwir_list.topk(320)
+        mask_vis = F.interpolate(mask_vis_list, scale_factor=[self.imgshape[0] / self.patch_num, self.imgshape[1] / self.patch_num], mode='nearest')
+        mask_lwir = F.interpolate(mask_lwir_list, scale_factor=[self.imgshape[0] / self.patch_num, self.imgshape[1] / self.patch_num], mode='nearest')
+        # mask_vis_list_topk ,topk_index_vis = mask_vis_list_.topk(320)
+        # mask_lwir_list_topk,topk_index_lwir = mask_lwir_list_.topk(320)
+        #
+        # mask_vis_list_min = torch.min(mask_vis_list_topk, dim=1).values
+        # mask_lwir_list_min = torch.min(mask_lwir_list_topk, dim=1).values
+        #
+        # # repeat里的4和x的最后一维相同
+        # mask_vis_list_min_= mask_vis_list_min.unsqueeze(-1)
+        # mask_lwir_list_min_= mask_lwir_list_min.unsqueeze(-1)
+        # ge_vis = torch.ge(mask_vis_list_, mask_vis_list_min_)
+        # ge_lwir = torch.ge(mask_lwir_list_, mask_lwir_list_min_)
+        # # mask_ = torch.where(ge_vis, mask_vis_list, torch.tensor(0.).cuda()).reshape(
+        # #     (-1, 1, self.patch_num, self.patch_num))
+        #
+        # # 设置zero变量，方便后面的where操作
+        # zero = torch.zeros_like(mask_vis_list_)
+        # one = torch.ones_like(mask_vis_list_)
+        # mask_vis_list = torch.where(ge_vis, one, zero).reshape((-1,1,self.patch_num,self.patch_num))
+        # mask_lwir_list = torch.where(ge_lwir, one, zero).reshape((-1,1,self.patch_num,self.patch_num))
+        # if self.keep_low:
+        #     mask_vis_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 1
+        #     mask_lwir_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 1
+        #
+        # else:
+        #     mask_vis_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 0
+        #     mask_lwir_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 0
+        # mask_vis = F.interpolate(mask_vis_list,
+        #                          scale_factor=[self.imgshape[0] / self.patch_num, self.imgshape[1] / self.patch_num],
+        #                          mode='nearest')
+        #
+        # mask_lwir = F.interpolate(mask_lwir_list,
+        #                          scale_factor=[self.imgshape[0] / self.patch_num, self.imgshape[1] / self.patch_num],
+        #                          mode='nearest')
 
-        mask_vis_list_min = torch.min(mask_vis_list_topk, dim=1).values
-        mask_lwir_list_min = torch.min(mask_lwir_list_topk, dim=1).values
-
-        # repeat里的4和x的最后一维相同
-        mask_vis_list_min_= mask_vis_list_min.unsqueeze(-1)
-        mask_lwir_list_min_= mask_lwir_list_min.unsqueeze(-1)
-        ge_vis = torch.ge(mask_vis_list, mask_vis_list_min_)
-        ge_lwir = torch.ge(mask_lwir_list, mask_lwir_list_min_)
-        # 设置zero变量，方便后面的where操作
-        zero = torch.zeros_like(mask_vis_list)
-        one = torch.ones_like(mask_vis_list)
-        mask_vis_list = torch.where(ge_vis, one, zero).reshape((-1,1,self.patch_num,self.patch_num))
-        mask_lwir_list = torch.where(ge_lwir, one, zero).reshape((-1,1,self.patch_num,self.patch_num))
-        if self.keep_low:
-            mask_vis_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 1
-            mask_lwir_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 1
-
-        else:
-            mask_vis_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 0
-            mask_lwir_list[:, :, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1, int(self.patch_num/2) - 1:int(self.patch_num/2) + 1] = 0
-        mask_vis = F.interpolate(mask_vis_list,
-                                 scale_factor=[self.imgshape[0] / self.patch_num, self.imgshape[1] / self.patch_num],
-                                 mode='nearest')
-        mask_lwir = F.interpolate(mask_lwir_list,
-                                 scale_factor=[self.imgshape[0] / self.patch_num, self.imgshape[1] / self.patch_num],
-                                 mode='nearest')
         # import cv2
         # import numpy as np
         # cv2.imwrite('/home/zhangguiwei/KK/codes/mmdet3-spectral/mmdet/test_vis.png',np.array(255*mask_vis[0].permute(1,2,0).cpu()))
         # cv2.imwrite('/home/zhangguiwei/KK/codes/mmdet3-spectral/mmdet/test_lwir.png',
         #             np.array(255 * mask_lwir[0].permute(1, 2, 0).cpu()))
+
         return mask_vis, mask_lwir
 
 
